7b7fa2d5ecfe57fcf2819e45b92cd9463fd8f7ef
Pavel Volchenko <2793553@gmail.com>
2022-12-01 06:15:09 +0300

    Init

---
+++ Seminar03-Task005.py
@@ -0,0 +1,26 @@
+""" Задайте число. Составьте список чисел Фибоначчи,
+    в том числе для отрицательных индексов.
+    Пример:
+    - для k = 8 список будет выглядеть так:
+    [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]  """
+
+
+def fib(x):
+    if x in [1, 2]:
+        return 1
+    elif x <= 0:
+        return fib(x + 2) - fib(x + 1)
+    else:
+        return fib(x - 1) + fib(x - 2)
+
+
+n = int(input('Enter a number: '))
+lst = []
+
+for i in range(-n, n + 1):
+    lst.append(fib(i))
+print(lst)
+
+
+
+
